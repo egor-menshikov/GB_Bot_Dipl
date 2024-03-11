ddd2222655e3813e48704db6ad433bca95294b9b
Egor Menshikov <r3born.fs@gmail.com>
2024-03-11 17:27:43 +0300

    created engine and session maker for DB, added launch and shutdown mechanisms.

--- app.py
+++ app.py
@@ -6,13 +6,13 @@ from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from dotenv import find_dotenv, load_dotenv
 
+load_dotenv(find_dotenv())
+
 from common.bot_cmds_list import private
+from database.engine import create_db, drop_db
 from handlers.user_private import user_private_rt
 from handlers.user_group import user_group_rt
 from handlers.admin_private import admin_rt
-
-
-load_dotenv(find_dotenv())
 
 ALLOWED_UPDATES = ['message', 'edited_message']
 
@@ -24,8 +24,22 @@ dp = Dispatcher()
 dp.include_routers(user_private_rt, user_group_rt, admin_rt)
 
 
+async def on_startup(bot):
+    run_param = False
+    if run_param:
+        await drop_db()
+
+    await create_db()
+
+
+async def on_shutdown(bot):
+    print('бот лег')
+
+
 # бот начинает слушать сервер тг
 async def main():
+    dp.startup.register(on_startup)
+    dp.shutdown.register(on_shutdown)
     await bot.delete_webhook(drop_pending_updates=True)
     await bot.set_my_commands(commands=private, scope=types.BotCommandScopeAllPrivateChats())
     # await bot.delete_my_commands(scope=types.BotCommandScopeAllPrivateChats())
