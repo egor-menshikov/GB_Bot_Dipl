242d2774946e305a9f2fa511a9f99d79ca49fe25
Egor Menshikov <r3born.fs@gmail.com>
2024-03-16 17:10:01 +0300

    added cart functionality

--- database/models.py
+++ database/models.py
@@ -29,7 +29,7 @@ class Product(Base):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     name: Mapped[str] = mapped_column(String(150), nullable=False)
     description: Mapped[str] = mapped_column(Text)
-    price: Mapped[float] = mapped_column(Numeric(5,2), nullable=False)
+    price: Mapped[float] = mapped_column(Numeric(5, 2), nullable=False)
     image: Mapped[str] = mapped_column(String(150))
     category_id: Mapped[int] = mapped_column(ForeignKey('category.id', ondelete='CASCADE'), nullable=False)
 
@@ -42,8 +42,8 @@ class User(Base):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     user_id: Mapped[int] = mapped_column(BigInteger, unique=True)
     first_name: Mapped[str] = mapped_column(String(150), nullable=True)
-    last_name: Mapped[str]  = mapped_column(String(150), nullable=True)
-    phone: Mapped[str]  = mapped_column(String(13), nullable=True)
+    last_name: Mapped[str] = mapped_column(String(150), nullable=True)
+    phone: Mapped[str] = mapped_column(String(13), nullable=True)
 
 
 class Cart(Base):
--- handlers/user_private.py
+++ handlers/user_private.py
@@ -33,7 +33,6 @@ async def add_to_cart(callback: types.CallbackQuery, callback_data: MenuCallBack
 
 @user_private_rt.callback_query(MenuCallBack.filter())
 async def user_menu(callback: types.CallbackQuery, callback_data: MenuCallBack, session: AsyncSession):
-
     if callback_data.menu_name == 'add_to_cart':
         await add_to_cart(callback, callback_data, session)
         return
@@ -44,41 +43,15 @@ async def user_menu(callback: types.CallbackQuery, callback_data: MenuCallBack, 
         menu_name=callback_data.menu_name,
         category=callback_data.category,
         page=callback_data.page,
+        product_id=callback_data.product_id,
+        user_id=callback.from_user.id,
     )
 
     await callback.message.edit_media(media=media, reply_markup=reply_markup)
     await callback.answer()
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# Старый код, чтобы не потерять.
 
 # from aiogram import types, Router, F
 # from aiogram.filters import CommandStart, Command, or_f
